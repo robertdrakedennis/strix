df1c01e2ddddfeaceb45f3480b6e707db8991de4
atlas <robertdennispersonal@gmail.com>
2020-05-26 03:07:37 -0400

    init nuxt

---
+++ resources/nuxt.config.js
@@ -0,0 +1,134 @@
+export default {
+    mode: 'spa',
+
+    env: {
+        version: pkg.version,
+    },
+
+    /*
+    ** Headers of the page
+    */
+    head: {
+        title: 'Quub',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Quub is the modern marketplace for web content' },
+            { hid: 'keywords', name: 'keywords', content: 'pterodactyl, ptero, ptero addons, quub, addons, resources, themes, development, software, digital content, hosting' },
+            { hid: 'og:title', name: 'og:title', content: 'Quub - Website' },
+            { hid: 'og:site_name', name: 'og:site_name', content: 'Quub' },
+            { hid: 'og:description', name: 'og:description', content: 'Quub is the modern marketplace for digital content' },
+            { hid: 'og:image', name: 'og:image', content: '/icon.png' },
+
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    // sitemap: {
+    //   routes() {
+    //     return getRoutes();
+    //   },
+    //   path: '/sitemap.xml',
+    //   gzip: true,
+    //   generate: false,
+    // },
+    router: {
+        middleware: [
+            'check-auth',
+        ],
+        linkActiveClass: 'active-link'
+    },
+
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: '~/components/PageLoader.vue',
+
+    /*
+    ** Global CSS
+    */
+    css: [
+        // 'vue-select/dist/vue-select.css'
+    ],
+
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+        '~plugins/permission-directive',
+        '~plugins/vue-tippy',
+        '~plugins/event-bus',
+        '~plugins/lazy',
+        '~plugins/vform',
+        '~plugins/axios',
+        '~plugins/modal',
+        '~plugins/vue-select',
+        '~plugins/nuxt-client-init',
+    ],
+
+    /*
+    ** Nuxt.js dev-modules
+    */
+    buildModules: [
+        // Doc: https://github.com/nuxt-community/eslint-module
+        // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+        '@nuxtjs/dotenv',
+        '@nuxtjs/tailwindcss'
+    ],
+
+    tailwindcss: {
+        configPath: '~/config/tailwind.config.js',
+        cssPath: '~/assets/styles/app.scss',
+        purge: false
+    },
+
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+        'nuxt-lazy-load',
+        '@nuxtjs/toast'
+        // '@nuxtjs/sitemap'
+    ],
+
+    toast: {
+        position: 'bottom-right',
+        theme: "outline",
+        duration : 99999
+    },
+
+    purgeCSS: {
+        mode: 'postcss',
+        enabled: false
+    },
+
+    /*
+    ** Axios module configuration
+    ** See https://axios.nuxtjs.org/options
+    */
+    axios: {
+
+    },
+    /*
+    ** Build configuration
+    */
+    build: {
+
+        postcss: {
+            plugins: [
+                postcssImport()
+            ]
+        },
+
+        /*
+        ** You can extend webpack config here.
+        */
+        extend (config, ctx) {
+            //
+        },
+    }
+}

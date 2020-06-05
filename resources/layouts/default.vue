608207c4065c8cb05acca89edfc7f89fc81a79ce
atlas <robertdennispersonal@gmail.com>
2020-06-05 04:48:08 -0400

    extract default.vue layout css

--- resources/layouts/default.vue
+++ resources/layouts/default.vue
@@ -4,18 +4,3 @@
         <nuxt />
     </div>
 </template>
-
-<style lang="scss">
-    html {
-        @apply relative h-full font-sans font-normal antialiased;
-
-        &::before {
-            z-index: -99999;
-            content: "";
-
-            background: radial-gradient(circle at top left, transparentize(#393947, 0.6) 0%, rgba(#393947, 0) 60%), theme('colors.neutral.700');
-
-            @apply fixed top-0 left-0 bg-cover bg-no-repeat bg-center h-full w-full;
-        }
-    }
-</style>

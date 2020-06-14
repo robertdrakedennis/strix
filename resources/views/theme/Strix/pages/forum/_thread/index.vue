e26f56c0f352b46acc14c41520c07436baf362c9
atlas <robertdennispersonal@gmail.com>
2020-06-13 20:12:32 -0400

    create forum\_thread\index

---
+++ resources/views/theme/Strix/pages/forum/_thread/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <backdrop image="https://i.imgur.com/KyviFID.jpg" />
+
+        <spacer x="10rem" />
+
+        <container class="space-y-6">
+            <div class="relative flex flex-row items-center justify-between">
+                <h6 class="text-neutral-050 font-bold text-3xl w-full max-w-2xl truncate">
+                    some stupidly long title soo i can see how legit insanely long this is
+                </h6>
+
+                <Button type="link" url="#reply" variant="default">
+                    <font-awesome-icon :icon="['fas', 'reply']" class="fa-fw fa-sm" />
+                </Button>
+            </div>
+
+            <card>
+                <forum-post />
+            </card>
+
+            <card>
+                <forum-post />
+            </card>
+
+            <card>
+                <forum-post />
+            </card>
+
+            <card>
+                <forum-post />
+            </card>
+        </container>
+
+        <spacer x="4rem" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style>
+
+</style>

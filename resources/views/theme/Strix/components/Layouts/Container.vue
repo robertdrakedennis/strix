8ad7a9dea12646f2f8079c4d4d9dcab4f238b998
atlas <robertdennispersonal@gmail.com>
2020-06-13 20:10:27 -0400

    update Container.vue

--- resources/views/theme/Strix/components/Layouts/Container.vue
+++ resources/views/theme/Strix/components/Layouts/Container.vue
@@ -1,15 +1,61 @@
 <template>
-    <div class="">
-
+    <div class="container responsive-container" :class="classList">
+        <slot />
     </div>
 </template>
 
 <script>
     export default {
-        name: "Container"
+        name: "Container",
+        props: {
+            flex: {
+                type: Boolean,
+                default: false,
+            },
+            flexCol: {
+                type: Boolean,
+                default: false
+            },
+            centered: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data () {
+            return {
+                classList: []
+            }
+        },
+        mounted() {
+            this.classList = [
+                this.flex ? 'container--flex' : null,
+                this.flexCol ? 'container--columns' : null,
+                this.centered ? 'container--centered' : null,
+            ];
+        }
     }
 </script>
 
-<style lang="css">
+<style>
+    .responsive-container {
+        @apply relative px-6;
+    }
 
+    @media (min-width: theme('screens.md')) {
+        .responsive-container {
+            @apply mx-auto px-0;
+        }
+    }
+
+    .container--flex {
+        @apply flex flex-row;
+    }
+
+    .container--columns {
+        @apply flex-col;
+    }
+
+    .container--centered {
+        @apply items-center justify-center;
+    }
 </style>

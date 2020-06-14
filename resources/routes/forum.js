3865009b6adde6c0de7454b01993766eb680def1
atlas <robertdennispersonal@gmail.com>
2020-06-13 20:10:59 -0400

    update forum.js

--- resources/routes/forum.js
+++ resources/routes/forum.js
@@ -6,4 +6,19 @@ export default [
         name: 'forum.index',
         component: page('forum/index.vue')
     },
+    {
+        path: '/forums/category/:category/:slug?',
+        name: 'forum.category.index',
+        component: page('forum/_category/index.vue')
+    },
+    {
+        path: '/forums/board/:board/:slug?',
+        name: 'forum.board.index',
+        component: page('forum/_board/index.vue')
+    },
+    {
+        path: '/forums/thread/:thread/:slug?',
+        name: 'forum.thread.index',
+        component: page('forum/_thread/index.vue')
+    },
 ];

902a0be5afc3b193eff3249c5a5c6a78f8c86093
atlas <robertdennispersonal@gmail.com>
2020-06-13 20:08:04 -0400

    comment morphs migration

---
+++ database/migrations/2020_06_09_101018_create_commentable_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentableTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commentables', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('comment_id');
+            $table->morphs('commentable');
+            $table->morphs('commenter');
+            $table->timestamps();
+
+            $table->unique(['comment_id', 'commentable_id', 'commentable_type'], 'commentable_ids_type_unique');
+            $table->unique(['commenter_id', 'commenter_type'], 'commenter_ids_type_unique');
+
+            $table->foreign('comment_id')
+                ->references('id')
+                ->on('comments')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commentable');
+    }
+}

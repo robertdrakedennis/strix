381e87d9a3b25bfe7be2c81e4bc92bba3564c9a3
atlas <robertdennispersonal@gmail.com>
2020-06-13 20:14:29 -0400

    create UsesJson.php model

---
+++ app/Traits/Database/UsesJson.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Strix\Traits\Database;
+
+
+use DB;
+use PDO;
+
+trait UsesJson
+{
+    /**
+     * The version to compare the server against.
+     *
+     * @var string
+     */
+    protected static string $version = '5.7.8';
+
+    /**
+     * Detects if we can use a json table for the most common DB engine.
+     * Thanks https://github.com/rinvex/laravel-categories for this snippet.
+     *
+     * @return string
+     */
+    public static function jsonable(): string
+    {
+        $driverName = DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME);
+
+        $dbVersion = DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
+
+        $isOldVersion = version_compare($dbVersion, static::$version, 'lt');
+
+        return $driverName === 'mysql' && $isOldVersion ? 'text' : 'json';
+    }
+}

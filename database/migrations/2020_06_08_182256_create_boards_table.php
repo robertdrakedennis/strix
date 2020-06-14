712a20b4e1dbcc8576fdba2be2bb928cba5c7807
atlas <robertdennispersonal@gmail.com>
2020-06-13 20:07:08 -0400

    boards migration

---
+++ database/migrations/2020_06_08_182256_create_boards_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    use \Strix\Traits\Database\UsesJson;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid', 21)->index();
+
+            $table->string('title');
+            $table->string('slug');
+            $table->{static::jsonable()}('description');
+            $table->timestamp('locked_at')->nullable();
+            $table->unsignedBigInteger('thread_count')->default(0);
+            $table->unsignedBigInteger('comment_count')->default(0);
+            $table->unsignedBigInteger('weight')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}

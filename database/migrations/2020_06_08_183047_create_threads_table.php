966cf2945b08ea9685d59fd252c86cdb08568a71
atlas <robertdennispersonal@gmail.com>
2020-06-13 20:07:34 -0400

    threads migration

---
+++ database/migrations/2020_06_08_183047_create_threads_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadsTable extends Migration
+{
+    use \Strix\Traits\Database\UsesJson;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid', 21)->index();
+
+            $table->string('title');
+            $table->string('slug');
+            $table->unsignedBigInteger('comment_count')->default(0);
+            $table->{static::jsonable()}('content');
+
+            $table->unsignedBigInteger('board_id');
+            $table->foreign('board_id')
+                ->references('id')
+                ->on('boards')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->timestamp('locked_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}

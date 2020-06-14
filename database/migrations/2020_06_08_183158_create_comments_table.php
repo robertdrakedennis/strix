eacd69bc58e0ee1092e1e1308279e7f54d38086f
atlas <robertdennispersonal@gmail.com>
2020-06-13 20:07:55 -0400

    comments migration

---
+++ database/migrations/2020_06_08_183158_create_comments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid', 21)->index();
+
+            $table->longText('content');
+            $table->unsignedBigInteger('commenter_id');
+            $table->string('commenter_type')->nullable();
+            $table->index(["commenter_id", "commenter_type"]);
+            $table->string("commentable_type");
+            $table->uuid("commentable_id");
+            $table->index(["commentable_type", "commentable_id"]);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}

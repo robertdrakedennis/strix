a6f71d25e52dd15b395f5c75851908e00e2b1ec1
atlas <robertdennispersonal@gmail.com>
2020-06-13 20:07:25 -0400

    categories migration

---
+++ database/migrations/2020_06_08_183020_create_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    use \Strix\Traits\Database\UsesJson;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid', 21)->index();
+
+            $table->string('title');
+            $table->string('slug');
+            $table->{static::jsonable()}('description')->nullable();
+            $table->unsignedBigInteger('weight')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
